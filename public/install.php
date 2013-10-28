9f1357689e529d54fa8b4cd2a08445d4009bd409
lukasni <niederbergerlukas@gmail.com>
2013-10-28 13:12:40 +0100

    Layout changes, added basic styling

--- public/install.php
+++ public/install.php
@@ -3,10 +3,12 @@
 $htaccess = __DIR__.DIRECTORY_SEPARATOR.'.htaccess';
 $config = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'global.php';
 $message = '';
+$prevent = true;
 
 if ( is_writable($htaccess) && is_writable($config) )
 {
 	$message .= '.htaccess and /config/global are is writable, automatic install possible!<br>';
+	$prevent = false;
 }
 else
 {
@@ -36,18 +38,20 @@ if ( array_key_exists('baseurl', $_POST) )
 <head>
 	<meta charset="UTF-8">
 	<title>Form Generator Installation</title>
+
+	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
 	<h1>Installation</h1>
 
-	<p class="message"><?=$message?></p>
+	<p class="message red"><?=$message?></p>
 
 	<p>Please check if the base url below is correct, edit if necessary. Press "Install" to install. You will be redirected to the index page.</p>
-	<form action="" method="post">
+	<form action="" method="post" class="install">
 		<fieldset>
 			<legend>Base URL</legend>
 			<input type="text" name="baseurl" value="<?=$baseurl?>">
-			<button>Install</button>
+			<button<?=$prevent?' disabled':''?>>Install</button>
 		</fieldset>
 	</form>
 
